ac5037f8054bd270284bb385cc24471b5f6268f7
AbdulKhaliq <abdul5ali8@gmail.com>
2018-05-13 13:43:26 +0300

    commit

--- ram/forms.py
+++ ram/forms.py
@@ -58,11 +58,11 @@ class EmpDataForm(ModelForm):
         fields = '__all__'
         widgets = {
             'emp_id':TextInput(attrs={'type':'number','maxlength':'10','class': 'form-control form-group','placeholder':'رقم الهوية','required':True}),
-            'emp_name':TextInput(attrs={'class': 'form-control form-group','placeholder':'الإسم الكامل','required':True}),
-            'emp_dept':TextInput(attrs={'class': "form-control",'placeholder':"الإدارة",'required':True}),
-            'emp_mobile':TextInput(attrs={'type':'number','class': "form-control",'placeholder':"الجوال",'required':True}),
-            'emp_ext':TextInput(attrs={'type':'number','class': "form-control",'placeholder':"هاتف المكتب",'required':True}),
-            'emp_email':TextInput(attrs={'type':'email','class': "form-control",'placeholder':"بريد العمل الإلكتروني",'required':True}),
+            'emp_name':TextInput(attrs={'class': 'form-control form-group','maxlength':'150','placeholder':'الإسم الكامل','required':True}),
+            'emp_dept':TextInput(attrs={'class': "form-control",'maxlength':'150','placeholder':"الإدارة",'required':True}),
+            'emp_mobile':TextInput(attrs={'type':'number','class': "form-control",'maxlength':'10','placeholder':"الجوال",'required':True}),
+            'emp_ext':TextInput(attrs={'type':'number','class': "form-control",'maxlength':'4','placeholder':"هاتف المكتب",'required':True}),
+            'emp_email':TextInput(attrs={'type':'email','class': "form-control",'maxlength':'50','placeholder':"بريد العمل الإلكتروني",'required':True}),
         }
 
 
